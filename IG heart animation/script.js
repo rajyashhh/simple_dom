e577ff7c0bf5facc981cc24d7d70fd223a7b0f4b
Yash Raj Singh <workmail.yrs@gmail.com>
2025-02-03 02:36:26 +0530

    insta like feature

---
+++ IG heart animation/script.js
@@ -0,0 +1,18 @@
+let like = document.querySelector("i");
+let cont =document.querySelector("#container")
+let ctr = 0;
+cont.addEventListener("dblclick",function(){
+    like.style.transform="translate(-50%,-50%) scale(2)";
+    ctr = ctr + 1;
+    console.log("heart added")
+    if(ctr == 1){
+        console.log("controller reached here")
+        setTimeout(removeHeart,1100);
+    }
+    function removeHeart(){
+        like.style.transform="translate(-50%,-50%) scale(0)";
+        ctr = ctr - 1;
+        console.log("heart removed")
+    }
+    
+})
